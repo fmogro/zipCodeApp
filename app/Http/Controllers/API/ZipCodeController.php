6318c36db584df963f7db7e98300a6d860d9758a
Felipe Mogro <fmogro@gmail.com>
2022-05-17 07:11:47 -0500

    cambios

--- app/Http/Controllers/API/ZipCodeController.php
+++ app/Http/Controllers/API/ZipCodeController.php
@@ -18,24 +18,23 @@ class ZipCodeController extends Controller{
 
         try{
             $data = ZipCode::where("d_codigo",$zip_code)->firstOrFail();
-
             
             if($data){
-                $infoC = $data->first();
-                $d_estado = mb_strtoupper($this->eliminarAcentos(html_entity_decode($infoC->d_estado)));
+      
+                $d_estado = mb_strtoupper($this->eliminarAcentos(html_entity_decode($data->d_estado)));
                 $federal_entity = new \stdClass();
-                $federal_entity->key  = intval(html_entity_decode($infoC->c_estado));
-                $federal_entity->name =  mb_strtoupper($this->eliminarAcentos(html_entity_decode($infoC->d_estado)));
-                if ($infoC->c_cp===""){
-                    $infoC->c_cp=null;
+                $federal_entity->key  = intval(html_entity_decode($data->c_estado));
+                $federal_entity->name =  mb_strtoupper($this->eliminarAcentos(html_entity_decode($data->d_estado)));
+                if ($data->c_cp===""){
+                    $data->c_cp=null;
                 }
-                $federal_entity->code = $infoC->c_cp;
+                $federal_entity->code = $data->c_cp;
     
                 $municipality = new \stdClass();
-                $municipality->key  = intval($this->eliminarAcentos(html_entity_decode($infoC->c_mnpio)));
+                $municipality->key  = intval($this->eliminarAcentos(html_entity_decode($data->c_mnpio)));
     
-                $d_mnpio = mb_strtoupper($this->eliminarAcentos(html_entity_decode($infoC->d_mnpio)));
-                $municipality->name = ($this->eliminarAcentos(html_entity_decode($d_mnpio)));
+                $d_mnpio = mb_strtoupper($this->eliminarAcentos(html_entity_decode($data->d_mnpio)));
+                $municipality->name = $this->eliminarAcentos(html_entity_decode($d_mnpio));
                
                 $settlements = collect();
     
@@ -47,16 +46,18 @@ class ZipCodeController extends Controller{
     
                 $settle = new \stdClass();
                 $settle->key                = intval(html_entity_decode($data->id_asenta_cpcons));
-                $settle->name               = $d_asenta;
+                $settle->name               = $this->eliminarAcentos($d_asenta);
                 $settle->zone_type          = mb_strtoupper($this->eliminarAcentos($data->d_zona));
                 $settle->settlement_type    = $settlement_type;
                 $settlements->push($settle);
                 
                 $result = new \stdClass();
 
-               
+                if (strlen($data->d_codigo)<5){
+                  $data->d_codigo = "0".$data->d_codigo;
+                }
   
-                $result->zip_code = ($data->d_codigo);
+                $result->zip_code = strval($data->d_codigo);
           
     
                 $locality = mb_strtoupper($this->eliminarAcentos(html_entity_decode($data->d_ciudad)));
@@ -78,11 +79,47 @@ class ZipCodeController extends Controller{
 
     }
 
-    public function eliminarAcentos($texto){
-        $replace = array('/é/','/í/','/ó/','/á/','/ñ/', '/ú/', '/ü/','/Á/','/É/','/Í/','/Ó/','/Ú/');
-        $remplazo = array('e','i','o','a', 'n', 'u', 'u','A','E','I','O','U');
-        $nuevaCadena = preg_replace($replace, $remplazo, $texto);
-        return $nuevaCadena;
+    public function eliminarAcentos($cadena){
+       //Reemplazamos la A y a
+		$cadena = str_replace(
+            array('Á', 'À', 'Â', 'Ä', 'á', 'à', 'ä', 'â', 'ª'),
+            array('A', 'A', 'A', 'A', 'a', 'a', 'a', 'a', 'a'),
+            $cadena
+            );
+    
+            //Reemplazamos la E y e
+            $cadena = str_replace(
+            array('É', 'È', 'Ê', 'Ë', 'é', 'è', 'ë', 'ê'),
+            array('E', 'E', 'E', 'E', 'e', 'e', 'e', 'e'),
+            $cadena );
+    
+            //Reemplazamos la I y i
+            $cadena = str_replace(
+            array('Í', 'Ì', 'Ï', 'Î', 'í', 'ì', 'ï', 'î'),
+            array('I', 'I', 'I', 'I', 'i', 'i', 'i', 'i'),
+            $cadena );
+    
+            //Reemplazamos la O y o
+            $cadena = str_replace(
+            array('Ó', 'Ò', 'Ö', 'Ô', 'ó', 'ò', 'ö', 'ô'),
+            array('O', 'O', 'O', 'O', 'o', 'o', 'o', 'o'),
+            $cadena );
+    
+            //Reemplazamos la U y u
+            $cadena = str_replace(
+            array('Ú', 'Ù', 'Û', 'Ü', 'ú', 'ù', 'ü', 'û'),
+            array('U', 'U', 'U', 'U', 'u', 'u', 'u', 'u'),
+            $cadena );
+    
+            //Reemplazamos la N, n, C y c
+            $cadena = str_replace(
+            array('Ñ', 'ñ', 'Ç', 'ç'),
+            array('N', 'n', 'C', 'c'),
+            $cadena
+            );
+            
+            return $cadena;
+
     }
 
     public function show(){
