cdfbf630ac5994a4a141413aaa0b235d01641148
Felipe Mogro <fmogro@gmail.com>
2022-05-17 02:29:14 -0500

    Se realizan cambios menores

--- app/Http/Controllers/API/ZipCodeController.php
+++ app/Http/Controllers/API/ZipCodeController.php
@@ -11,33 +11,33 @@ class ZipCodeController extends Controller{
     public function obtenerZipCode($zip_code){
         $data = ZipCode::where("d_codigo",$zip_code)->firstOrFail();
             if($data){
-                $d_estado = mb_strtoupper((html_entity_decode($data->d_estado)));
+                $d_estado = mb_strtoupper($this->eliminarAcentos(html_entity_decode($data->d_estado)));
                 $federal_entity = new \stdClass();
                 $federal_entity->key  = intval(html_entity_decode($data->c_estado));
-                $federal_entity->name =  mb_strtoupper(html_entity_decode($data->d_estado));
+                $federal_entity->name =  mb_strtoupper($this->eliminarAcentos(html_entity_decode($data->d_estado)));
                 if ($data->c_cp===""){
                     $data->c_cp=null;
                 }
                 $federal_entity->code = $data->c_cp;
     
                 $municipality = new \stdClass();
-                $municipality->key  = intval(html_entity_decode($data->c_mnpio));
+                $municipality->key  = intval($this->eliminarAcentos(html_entity_decode($data->c_mnpio)));
     
-                $d_mnpio = mb_strtoupper( html_entity_decode($data->d_mnpio));
-                $municipality->name = html_entity_decode($d_mnpio);
-    
+                $d_mnpio = mb_strtoupper($this->eliminarAcentos(html_entity_decode($data->d_mnpio)));
+                $municipality->name = ($this->eliminarAcentos(html_entity_decode($d_mnpio)));
+               
                 $settlements = collect();
     
               
                 $settlement_type = new \stdClass();
-                $settlement_type->name = $data->d_tipo_asenta;
+                $settlement_type->name = $this->eliminarAcentos($data->d_tipo_asenta);
 
-                $d_asenta = mb_strtoupper(html_entity_decode($data->d_asenta));
+                $d_asenta = mb_strtoupper($this->eliminarAcentos(html_entity_decode($data->d_asenta)));
     
                 $settle = new \stdClass();
                 $settle->key                = intval(html_entity_decode($data->id_asenta_cpcons));
                 $settle->name               = $d_asenta;
-                $settle->zone_type          = mb_strtoupper($data->d_zona);
+                $settle->zone_type          = mb_strtoupper($this->eliminarAcentos($data->d_zona));
                 $settle->settlement_type    = $settlement_type;
                 $settlements->push($settle);
                 
@@ -50,7 +50,7 @@ class ZipCodeController extends Controller{
                 $result->zip_code = strval($data->d_codigo);
           
     
-                $locality = mb_strtoupper(html_entity_decode($data->d_ciudad));
+                $locality = mb_strtoupper($this->eliminarAcentos(html_entity_decode($data->d_ciudad)));
     
                 $result->locality       = $locality;
                 $result->federal_entity = $federal_entity;
@@ -61,5 +61,12 @@ class ZipCodeController extends Controller{
                return response()->json($result);
             }
         return response()->json($data);
+    }
+
+    public function eliminarAcentos($texto){
+        $replace = array('/é/','/í/','/ó/','/á/','/ñ/', '/ú/', '/ü/','/Á/','/É/','/Í/','/Ó/','/Ú/');
+        $remplazo = array('e','i','o','a', 'n', 'u', 'u','A','E','I','O','U');
+        $nuevaCadena = preg_replace($replace, $remplazo, $texto);
+        return $nuevaCadena;
     }
 }
